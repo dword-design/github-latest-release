026e27644c5fde41c5c5672c947d8ff8d67733fd
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-12-12 23:48:51 +0100

    fix: foo

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'wxt';
 
 export default defineConfig({
+  autoIcons: { developmentIndicator: false },
   manifest: {
     host_permissions: ['https://api.github.com/*'],
     name: 'GitHub Latest Release',
   },
+  modules: ['@wxt-dev/auto-icons'],
 });
