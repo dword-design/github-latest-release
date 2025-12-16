38cb8bed4e4b7fc185071e5b2ecb8e46ee389c29
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-12-16 11:34:22 +0100

    fix: fix tests

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,5 +1,5 @@
 import { handleError } from '@dword-design/github-web-extension-utils';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 export default defineContentScript({
   main: async () => {
@@ -18,12 +18,6 @@ export default defineContentScript({
     const [author, name] = scopedName.split('/');
 
     try {
-      const {
-        data: { tag_name: latestRelease },
-      } = await github.get(`/repos/${author}/${name}/releases/latest`, {
-        ...(token && { headers: { Authorization: `token ${token}` } }),
-      });
-
       const headline =
         document.querySelector('[itemprop=name]')!.parentElement!;
 
@@ -35,11 +29,32 @@ export default defineContentScript({
         latestReleaseSpan.remove();
       }
 
+      const {
+        data: { tag_name: latestRelease },
+      } = await github
+        .get(`/repos/${author}/${name}/releases/latest`, {
+          ...(token && { headers: { Authorization: `token ${token}` } }),
+        })
+        .catch(error => {
+          if (error instanceof AxiosError && error.response?.status === 404) {
+            return { data: { tag_name: null } };
+          }
+
+          throw error;
+        });
+
       latestReleaseSpan = document.createElement('span');
-      latestReleaseSpan.textContent = latestRelease;
-      latestReleaseSpan.classList.add('github-latest-release', 'Label');
+      latestReleaseSpan.classList.add('github-latest-release');
+
+      if (latestRelease) {
+        latestReleaseSpan.textContent = latestRelease;
+        latestReleaseSpan.classList.add('Label');
+      }
+
       headline.append(latestReleaseSpan);
     } catch (error) {
+      console.log('foo');
+
       handleError(error, {
         name: 'GitHub Latest Release',
         slug: 'github-latest-release',
--- index.spec.ts
+++ index.spec.ts
@@ -2,16 +2,16 @@ import { test } from '@dword-design/playwright-fixture-web-extension';
 import { expect } from '@playwright/test';
 import { execaCommand } from 'execa';
 
-test.beforeAll(() =>
-  execaCommand('base build', { env: { NODE_ENV: '' }, stdio: 'inherit' }),
-);
+test.beforeAll(() => execaCommand('base build', { stdio: 'inherit' }));
 
 test('no release', async ({ page }) => {
   await page.goto('https://github.com/github-latest-release/repo2');
+  await expect(page.locator('.github-latest-release')).toBeAttached();
   await expect(page.locator('#repository-container-header')).toHaveScreenshot();
 });
 
 test('works', async ({ page }) => {
   await page.goto('https://github.com/github-latest-release/repo1');
+  await expect(page.locator('.github-latest-release.Label')).toBeVisible();
   await expect(page.locator('#repository-container-header')).toHaveScreenshot();
 });
